017a180896e9931f25028e6c37e4413e846548c6
Keith Packard <keithp@keithp.com>
2025-06-06 08:41:31 -0700

    cmake: Support both toolchain directory layouts
    Until https://github.com/zephyrproject-rtos/sdk-ng/pull/936 is merged,
    SDK 0.18 is incompatible with previous versions as the paths to
    find the cmake bits is different.
    
    Deal with that by checking for files in the wrong place as well as the
    right one.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- cmake/toolchain/zephyr/generic.cmake
+++ cmake/toolchain/zephyr/generic.cmake
@@ -1,6 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
-include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/generic.cmake)
+if(EXISTS ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/gnu/generic.cmake)
+  include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/gnu/generic.cmake)
+else()
+  include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/generic.cmake)
+endif()
 
 set(TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
 
--- cmake/toolchain/zephyr/target.cmake
+++ cmake/toolchain/zephyr/target.cmake
@@ -1,3 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/target.cmake)
+if(EXISTS ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/gnu/target.cmake)
+  include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/gnu/target.cmake)
+else()
+  include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/target.cmake)
+endif()
