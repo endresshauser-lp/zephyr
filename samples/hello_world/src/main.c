8dd6f29f0f4452fc42351b35b0062289a09695b0
Tim Pambor <tim.pambor@codewrights.de>
2025-10-27 11:26:41 +0100

    Flash Test Firmware

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -8,7 +8,29 @@
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	printf("\n");
+	printf("Flash Test Firmware v7\n");
+	printf("\n");
+	printf("=== NAND ===\n");
+	printf(" == Read == \n");
+	printf("* Read and display 32 bytes from address c0: flash read nand c0 20\n");
+	printf("  Note: This commands reads a full page (2k) for every 16 bytes requested\n");
+	printf("\n");
+	printf("* Read 4096 bytes from address 0 once: flash read_test nand 0 4096 1\n");
+	printf("  Note: This command does only **one** request per page (2k).\n");
+	printf("\n");
+	printf(" == Erase == \n");
+	printf("* Erase bank 0 (128k): flash erase nand 0\n");
+	printf("* Erase bank 1 (128k): flash erase nand 20000\n");
+	printf("\n");
+	printf(" == Write == \n");
+	printf("* Write first page (2k) with increasing pattern 00 01 02 ...: flash write_test nand 0 2048 1\n");
+	printf("* Write second page (2k) with increasing pattern 00 01 02 ...: flash write_test nand 800 2048 1\n");
+	printf("\n");
+	printf(" == Erase + Write == \n");
+	printf("* Erase and write bank 0 once: flash erase_write_test nand 0 131072 1\n");
+	printf("* Erase and write bank 0 twice: flash erase_write_test nand 0 131072 2\n");
+	printf(" !! Caution: This may destroy your flash quickly!\n");
 
 	return 0;
 }
